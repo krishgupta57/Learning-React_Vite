e6947e904721b251f5c4a4b2abeaea779e38951f
krishgupta57 <krishgupta7898@gmail.com>
2025-11-11 13:09:50 +0530

    Mapping Image

---
+++ image/src/App.jsx
@@ -0,0 +1,13 @@
+import myimage from './assets/react.svg'
+
+function App(){
+  return(
+    <>
+      <img src="vite.svg" alt="" />
+      <img src={myimage} alt="" />
+    </>
+  )
+}
+
+
+export default App
