6960961294d2250d671422ae271b3cf74818f073
krishgupta57 <krishgupta7898@gmail.com>
2025-11-25 20:03:17 +0530

    Context API

--- Context API/src/Child2.jsx
+++ Context API/src/Child2.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import { data } from './App'
+import Child3 from './Child3'
+
+let data2 = createContext()
+let name = "Another data"
 
 const Child2 = () => {
   return (
@@ -9,8 +13,14 @@ const Child2 = () => {
                 (reciver)=>{return <h1>{reciver}</h1> }
             }
         </data.Consumer>
+
+        <data2.Provider value={name}>
+            <Child3 />
+        </data2.Provider>
     </>
   )
 }
 
 export default Child2
+
+export {data2}
